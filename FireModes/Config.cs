d92feaa588764b17269f3382010748c3785c009b
DereWah <daviloca03@gmail.com>
2023-09-24 13:43:52 +0200

    Update default config

--- FireModes/Config.cs
+++ FireModes/Config.cs
@@ -1,6 +1,7 @@
 ﻿using Exiled.API.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,14 @@ namespace FireModes
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
-
+        [Description("List of weapons that will work with FireModes")]
         public List<ItemType> FireModeWeapons { get; set; } = new List<ItemType>()
         {
             ItemType.GunE11SR,
-            ItemType.GunAK
+            ItemType.GunAK,
+            ItemType.GunCrossvec,
+            ItemType.GunFSP9,
+            ItemType.GunFRMG0
         };
 
     }
